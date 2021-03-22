7fe4e222e3708b90f5b53d323b73fa344d5c2c81
sumeikom <sumeikom@gmail.com>
2021-03-21 21:16:00 -0400

    commit tree 45bed84f2ba509151e153f2f4452f57804e774ff

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,32 @@
+class Genre
+  attr_accessor :name
 
+  def initialize(name)
+      @name = name
+      @@all << self
+  end
+
+  @@all = []
+
+  def self.all
+      @@all
+  end
+
+  def songs
+      Song.all.select {|song| song.genre == self}
+  end
+
+  def artists
+      genre_artists = []
+      Song.all.each do |song| 
+        if song.genre == self
+          genre_artists << song.artist
+        end
+      end
+      genre_artists
+    end
+
+
+      
+
+end
